e57df5dfd684c5c0aef5894f32f80bd04a460ad6
鹿乃まほろ / Mahoro Kano <88894099+kanomahoro@users.noreply.github.com>
2021-10-06 19:32:19 +0800

    Update auto_token.py

--- guest/auto_token.py
+++ guest/auto_token.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 import json
 driver=webdriver.PhantomJS()
-driver.get('https://mobile.twitter.com/kanomahoro')
+driver.get('https://mobile.twitter.com/Twitter')
 data=json.dumps(driver.get_cookie('gt'))
 with open('token.json','w',encoding='utf-8') as fp:
     fp.write(data)
